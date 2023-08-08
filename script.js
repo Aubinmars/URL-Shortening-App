4045354e226075c4eb1a20517fa31559802f0fd3
Aubinmars <aubinmars13@gmail.com>
2023-08-08 11:45:20 +0200

    added copy button to the app

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const shortenForm = document.getElementById('shorten-form');
 const urlInput = document.getElementById('url-input');
 const shortenedLinksContainer = document.getElementById('shortened-links');
 
+// Load existing shortened links from localStorage
+const storedLinks = JSON.parse(localStorage.getItem('shortenedLinks')) || [];
+storedLinks.forEach(link => addToShortenedLinks(link.originalUrl, link.shortenedUrl));
+
 shortenForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const originalUrl = urlInput.value;
@@ -19,6 +23,10 @@ shortenForm.addEventListener('submit', async (e) => {
             const shortenedUrl = data.result.full_short_link;
             addToShortenedLinks(originalUrl, shortenedUrl);
             urlInput.value = '';
+
+            // Save updated shortened links to localStorage
+            const updatedLinks = [...storedLinks, { originalUrl, shortenedUrl }];
+            localStorage.setItem('shortenedLinks', JSON.stringify(updatedLinks));
         } else {
             alert('Error shortening the URL.');
         }
@@ -34,6 +42,21 @@ function addToShortenedLinks(originalUrl, shortenedUrl) {
     linkElement.innerHTML = `
         <p><strong>Original URL:</strong> ${originalUrl}</p>
         <p><strong>Shortened URL:</strong> <a href="${shortenedUrl}" target="_blank">${shortenedUrl}</a></p>
+        <button class="copy-btn">Copy</button>
     `;
     shortenedLinksContainer.appendChild(linkElement);
+
+    // Add click event listener to copy button
+    const copyBtn = linkElement.querySelector('.copy-btn');
+    copyBtn.addEventListener('click', () => copyToClipboard(shortenedUrl));
+}
+
+function copyToClipboard(text) {
+    const tempInput = document.createElement('input');
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    alert('Copied to clipboard!');
 }
